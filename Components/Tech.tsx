a29d8ec2210721eba4aa5673a3adceef1069a73c
Abdullah Al Sayed <sayedabdullahal11@gmail.com>
2026-09-13 10:07:17 +0600

    Handler functiont added

--- Components/Tech.tsx
+++ Components/Tech.tsx
@@ -2,19 +2,24 @@ import { use } from "react";
 import type { Data } from "./Types/type";
 interface dataProps{
   TechData:Promise<Data[]>
-   
-    }
-    export default function Tech({ TechData }:dataProps) {
+   add: (tech:Data) => void,
+   selectedtech: Data[]
+}
+export default function Tech({ TechData,add,selectedtech }:dataProps) {
     const showdata=use(TechData)
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {showdata.map((tech: Data) => {
+              const isSelected = selectedtech.filter((t) => t.id === tech.id).length>0;
               return (
                 <div
                   key={tech.id} 
                   className={`rounded-2xl p-5 shadow-xs 
                      bg-white flex flex-col justify-between transition-all duration-150
-                     `}
+                     ${isSelected
+                         ? "border-2 border-pink-200 bg-pink-50"
+                         : "border border-gray-100 hover:border-gray-200"
+                     }`}
                 >
                   <div>
                     <div className="flex items-center justify-between">
@@ -41,11 +46,16 @@ interface dataProps{
                     </div>
                     <button
                       type="button"
-                      
+                      onClick={() => add(tech)}
+                      disabled={isSelected}
                       className={`w-full mt-4 text-sm font-semibold py-3 rounded-xl transition duration-150 shadow-xs cursor-pointer
+                        ${isSelected
+                            ? "bg-white text-gray-400 border border-gray-200 cursor-not-allowed opacity-70"
+                            : "bg-gradient-to-r from-[#e60067] to-[#f43f5e] hover:opacity-95 text-white"
+                        }
                       `}
                     >
-                      Add to Stack
+                      {isSelected ? "✓ Added to Stack" : "Add to Stack"}
                     </button>
                   </div>
                 </div>
